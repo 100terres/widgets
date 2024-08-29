9f1ca0ef9c25efdbe75d5a72245b0a7336d36d17
Gabriel Santerre <gab@100terres.com>
2024-08-28 23:59:37 -0400

    simplify build script and increase supported pnpm version

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -1,7 +1,4 @@
 # !/bin/bash
 
-# Make sure to install required pnpm version
-pnpm install -g pnpm@$(grep '"pnpm": "^' package.json | sed -E 's/.*"pnpm": ".*([0-9]\.[0-9]\.[0-9])"/\1/')
-
 # Run the build
 pnpm run build
