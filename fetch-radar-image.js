748e4b6e94c06a66e2f6b0e4770e144a02c0cb74
Gab <gab@100terres.com>
2025-09-10 22:58:55 -0400

    chore: switch from node/pnpm to deno

--- fetch-radar-image.js
+++ fetch-radar-image.js
@@ -1,6 +1,6 @@
-import path from "path";
-import fs from "fs";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
 
 function run() {
   const __filename = fileURLToPath(import.meta.url);
