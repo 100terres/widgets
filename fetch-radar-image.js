5910b8dd6487b86beee4814d5b664b18dd8206c8
Gab <gab@100terres.com>
2025-09-29 22:02:30 -0400

    chore: migrate to deno (#18)
    * chore: switch from node/pnpm to deno
    
    * chore: bump deno
    
    * ci: drop github action cron deployment

--- fetch-radar-image.js
+++ fetch-radar-image.js
@@ -1,6 +1,6 @@
-import path from "path";
-import fs from "fs";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
 
 function run() {
   const __filename = fileURLToPath(import.meta.url);
@@ -10,6 +10,7 @@ function run() {
     "./src/assets/generated/downloaded-radar.png",
   );
 
+  // eslint-disable-next-line no-undef
   fetch(
     "https://geo.meteo.gc.ca/geomet?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=43.256666847747745,-76.77304180512725,47.761171352252255,-70.34536499487275&CRS=EPSG:4326&WIDTH=600&HEIGHT=600&LAYERS=RADAR_1KM_RRAI&FORMAT=image/png&TRANSPARENT=true",
   )
@@ -17,6 +18,7 @@ function run() {
       return radarResult.arrayBuffer();
     })
     .then((arrayBuffer) => {
+      // eslint-disable-next-line no-undef
       const buffer = Buffer.from(arrayBuffer);
       fs.mkdirSync(path.dirname(destPath), { recursive: true });
       fs.writeFileSync(destPath, buffer);
